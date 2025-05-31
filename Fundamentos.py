b43b4d1ed15399b9f03e70a0f5cf6b242c1249e2
Miguel Cuevas <cuevasfm@gmail.com>
2025-05-31 13:01:40 -0600

    Agregaron operadores
    Aun falta agregar mas operadores

---
+++ Fundamentos.py
@@ -0,0 +1,34 @@
+'''
+Operadores aritméticos
+1HAcer tablas de verdad
+2Ejemplo de cada operador de comparación
+'''
+print("----- Operadores aritméticos -----")
+print("Operador suma 5 + 6 = ", 5 + 6) # Suma 5 y 6
+print("Operador resta 5 - 6 = ", 5 - 6) # Resta 5 y 6
+print("Operador multiplicación 5 * 6 = ", 5 * 6) # Multiplica 5 por 6
+print("Operador división 5 / 6 = ", 5 / 6) # Divide 5 por 6
+print("Operador módulo 5 % 6 = ", 5 % 6) # Devuelve el resto de la división
+print("Operador potencia 5 ** 6 = ", 5 ** 6) # 5 elevado a 6
+print("Operador división entera 5 // 6 = ", 5 // 6) # División entera, devuelve el cociente de la división
+print("Operador de cambio de signo -5 = ", -5)  # Cambia el signo de 5 a -5
+print("-----------------------------------")
+
+print("----- Operadores lógicos -----")
+print("Operador and")
+print("Operador and True and True = ", True and True) # Devuelve True si ambas expresiones son True, False en caso contrario
+print("Operador and True and False = ", True and False) # Devuelve True si ambas expresiones son True, False en caso contrario
+print("Operador and False and True = ", False and True) # Devuelve True si ambas expresiones son True, False en caso contrario
+print("Operador and False and False = ", False and False) # Devuelve True si ambas expresiones son True, False en caso contrario
+print("-----------------------------------")
+print("Operador or")
+print("Operador or True or True = ", True or True) # Devuelve True si alguna de las expresiones es True, False en caso contrario
+print("Operador or True or False = ", True or False) # Devuelve True si alguna de las expresiones es True, False en caso contrario
+print("Operador or False or True = ", False or True) # Devuelve True si alguna de las expresiones es True, False en caso contrario
+print("Operador or False or False = ", False or False) # Devuelve True si alguna de las expresiones es True, False en caso contrario
+print("Operador not True = ", not True) # Devuelve True si la expresión es False, False en caso contrario
+print("-----------------------------------")
+
+print("----- Operadores de comparación -----")
+print("Operador igual que 5 == 6 = ", 5 == 6) # Devuelve True si 5 es igual a 6, False en caso contrario
+print("Operador diferente que 5 != 6 = ", 5 != 6) # Devuelve True si 5 es diferente a 6, False en caso contrario
--- practica1.py
+++ practica1.py
@@ -2,3 +2,4 @@ numero1 = 9
 numero2 = 18
 resultado = numero1 + numero2
 print(resultado)
+
